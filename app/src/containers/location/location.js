440624b0bd964e0efc238f71197ca2b96ed849a5
Andrii <andrii.voitovych@kindgeek.com>
2019-02-01 14:19:08 +0200

    rewritten class component location in hook style

--- app/src/containers/location/location.js
+++ app/src/containers/location/location.js
@@ -1,10 +1,73 @@
-import React, { Component, Fragment } from 'react';
+import React, {
+  Component,
+  Fragment,
+  useState,
+  useEffect,
+  useRef,
+} from 'react';
 
 import Map from './map/map';
 import { NearbyPits } from './../../components';
 import texts from './../../helper/texts';
 
-class IndexPage extends Component {
+function IndexPage() {
+  const [pits, setPits] = useState([]);
+  const [lat, setLat] = useState('');
+  const [lng, setLng] = useState('');
+
+  const position = {
+    lat: lat,
+    lng: lng,
+  }
+
+  let nearby = useRef();
+
+  const regionFiltered = pits =>
+    nearby.current.updatePeople(pits);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const showPosition = position => {
+        setLat(position.coords.latitude);
+        setLng(position.coords.longitude);
+        setPits(texts.pits);
+      };
+      navigator.geolocation &&
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }, 1000)
+  }, [lat, lng, pits]);
+
+  return (
+    <main className="container-fluid position-absolute h-100 bg-light">
+      <div className="row position-absolute w-100 h-100">
+        <section className="col-md-9 px-0 border-right border-gray position-relative h-100">
+          {pits[0] && (
+            <Map
+              position={position}
+              radius={1000}
+              pits={pits}
+              onRegionFiltered={regionFiltered}
+            />
+          )}
+        </section>
+
+        <section className="col-md-3 position-relative d-flex flex-wrap h-100 align-items-start align-content-between bg-white px-0">
+          <NearbyPits
+            ref={nearby}
+            person={ position }
+          />
+        </section>
+      </div>
+    </main>
+  );
+
+
+
+}
+
+export default IndexPage;
+
+/*class IndexPage extends Component {
   state = {
     pits: [],
     lat: '',
@@ -71,4 +134,4 @@ class IndexPage extends Component {
   }
 }
 
-export default () => <IndexPage />;
+export default () => <IndexPage />;*/
